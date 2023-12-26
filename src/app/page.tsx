a0e7740791f9846ab9c5901f64505b4b8b78e50a
Nathaniel <nathaniel.davidson@gmail.com>
2023-12-25 20:47:31 -0700

    change description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,7 +86,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Projects currently using NucleoDB
+            Projects utilizing NucleoDB
           </p>
         </a>
       </div>
