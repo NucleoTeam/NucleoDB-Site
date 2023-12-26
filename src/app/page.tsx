c346f330f7f0d4683f3e68ab7ad70449df660cca
Nathaniel <nathaniel.davidson@gmail.com>
2023-12-25 20:19:05 -0700

    remove bottom gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <p className="lg:static lg:w-auto  lg:p-4">
           NucleoDB
         </p>
-        <div className="topMenu fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <div className="topMenu fixed bottom-0 left-0 flex h-48 w-full items-end justify-center lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a href={"https://docs.synload.com/books/nucleodb"}>&lt;NucleoDB Docs&gt;</a> <a href={"https://docs.synload.com/books/nucleodb/chapter/spring-data-repository-library"}>&lt;Spring Docs&gt;</a>
         </div>
       </div>
