0ed9638df097af0a2318077ada93a3ad7127a9e5
Nathaniel <nathaniel.davidson@gmail.com>
2023-12-27 17:28:28 -0700

    add memcached server

--- src/app/usages/page.tsx
+++ src/app/usages/page.tsx
@@ -27,6 +27,19 @@ export default function Home() {
                     </p>
                 </a>
                 <a
+                    href="https://docs.synload.com/books/nucleodb-memcached"
+                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                    target={"_blank"}
+                    rel="noopener noreferrer"
+                >
+                    <h2 className={`mb-3 text-2xl font-semibold`}>
+                        NucleoDB-Memcached{' '}
+                    </h2>
+                    <p className={`m-4 max-w-[30ch] text-sm opacity-50`}>
+                        Memcached server using NucleoDB
+                    </p>
+                </a>
+                <a
                     href="https://thejointforge.com"
                     className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
                     target={"_blank"}
